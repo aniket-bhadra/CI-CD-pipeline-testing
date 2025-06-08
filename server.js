c63a896e392fd2db78fa0c4f9194767c45de47d5
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-06-08 11:36:45 +0530

    created very basic node app to test ci cd pipeline

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  return res.json({
+    message: "nodejs application running inside container v2",
+  });
+});
+
+app.listen(PORT, () => console.log(`server is running at Port:${PORT}`));
